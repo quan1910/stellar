06dfa81c833bfd80f023b2da06ecbff1af98bd73
Nguyen Minh Quan <v.quannm16@vinid.net>
2020-11-15 19:08:42 +0700

    [Setup] Color literal

---
+++ stellar/Services/Colors.swift
@@ -0,0 +1,17 @@
+//
+//  Color.swift
+//  stellar
+//
+//  Created by Nguyen Minh Quan on 11/15/20.
+//
+
+import Foundation
+import UIKit
+
+// MARK: - Colors Literal
+enum Colors {
+    static let nah = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+    static let like = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1)
+    static let cardBackground = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+    static let shadow = #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)
+}
